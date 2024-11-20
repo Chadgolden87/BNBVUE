dc8737bc3843d007b85a2ec9351f79bb00a8865b
Chad <chad.golden87@gmail.com>
2024-11-20 14:46:45 -0500

    BNB VUE

--- vite.config.js
+++ vite.config.js
@@ -4,25 +4,25 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     }
-//   }
-// })
-
 export default defineConfig({
-  server: {
-    proxy: {
-      '/api': {
-        target: 'https://bnb-express.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-      },
-    },
-  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
+
+// export default defineConfig({
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'https://bnb-express.vercel.app',
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ''),
+//       },
+//     },
+//   },
+// })
